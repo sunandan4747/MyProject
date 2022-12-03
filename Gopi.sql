2718524469ef329eba15dd9d688b3010715089ab
sunanadan4747 <sunandanborse7@gmail.com>
2022-12-03 11:03:35 +0530

    xyzmsg

---
+++ Gopi.sql
@@ -0,0 +1,10 @@
+create table Gopi1 (Cust_id number(5),Cust_name char(10), Cust_surname char(10), Cust_sal number(7),Cust_dept char(7));
+describe Gopi1;
+
+
+insert into Gopi1 values(1001,'Laxman','Borse',50000,'Bcom');
+insert into Gopi1 values(1002,'Sunanda','Patil',99000,'Mcom');
+insert into Gopi1 values(1003,'Snehal', 'Bhosle',90000,'Msc');
+insert into Gopi1 values(1004,'Sumit','Patil',80000,'Phd');
+insert into Gopi1 values(1005,'Sakshi','Shewale',70000,'Bsc');
+select*from Gopi1;
